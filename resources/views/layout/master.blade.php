9e61566fe8277ec64abed3733edc502a03b02ebf
miguelangelflorezpantoja <miguelangelflorezpantoja@gmail.com>
2020-12-18 09:24:07 -0500

    nueva actualización

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -32,7 +32,7 @@
             
     </body>
 
-    <footer class="page-footer font-small black special-color-dark">
+    <footer class="page-footer font-small black special-color-Success">
     <div class="container-fluid text-center text-md-left">
         <div class="row">
             <div class="col-md-6 mt-md-0 mt-3">
