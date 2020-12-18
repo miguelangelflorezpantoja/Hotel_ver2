f5ca12a02f6122f5d358ea167c64b0bc5ac819b6
miguelangelflorezpantoja <miguelangelflorezpantoja@gmail.com>
2020-12-18 10:18:50 -0500

    nueva actualización

--- resources/views/contactos.blade.php
+++ resources/views/contactos.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.master')
-    @section('content')<h1>Listado contactos</h1>
+    @section('content')
 
 
 
+    <center><img src="   https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvDkxcAi_Ou-7zSD8g8PuOwAIFl9T04pzuGA&usqp=CAU "></center>
+    <center><div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Lineas de atención </div>
+  <div class="card-body">
+    
+    <p class="card-text">300 305 02 95. </p>
+    <p class="card-text">300 305 456 </p>
+  </div>  </center>
 @stop
--- resources/views/facturacion.blade.php
+++ resources/views/facturacion.blade.php
@@ -1,6 +1,10 @@
 @extends('layout.master')
     @section('content')
-<h1>Facturacion</h1>
+<h1></h1>
 
 
+<center><img src="https://aescolombia.com.co/wp-content/uploads/2019/07/facturacion_electronica.png "></center>
+
+
+<center><button type="button" class="btn btn-primary btn-lg">Generar Factura</button></center>
 @stop
