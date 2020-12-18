bc77b1742c48366e292165b3b64d10a98083f0d9
miguelangelflorezpantoja <miguelangelflorezpantoja@gmail.com>
2020-12-18 10:04:13 -0500

    nueva actualización

--- resources/views/reservas.blade.php
+++ resources/views/reservas.blade.php
@@ -1,6 +1,18 @@
 @extends('layout.master')
     @section('content')
-<h1>rrvas</h1>
+<center><h1>Reserva a qui tu habitacion !!!!</h1></center>
+<div class="mb-3">
+<center><label for="exampleFormControlInput1" class="">Correo electronico</label></center>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
+<div class="mb-3">
+<center> <label for="exampleFormControlInput1" class="">Clave</label></center> 
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
 
-
+<div class="mb-3">
+<center>  <label for="exampleFormControlTextarea1" class="form-label">Observaciones</label></center> 
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<center> <button type="button" class="btn btn-primary btn-lg">Entrar</button></center> 
 @stop
